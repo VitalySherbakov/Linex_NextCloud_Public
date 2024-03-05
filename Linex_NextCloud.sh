cca2cf62e818426bbc35579eb9cbf92411fd5a28
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2024-03-05 12:46:43 +0200

    98489

---
+++ Linex_NextCloud.sh
@@ -0,0 +1,182 @@
+versionscript=1.014
+echo "Установка и Работа Linex Web Public SV (Щ.В) (v $versionscript)"
+distributivelinex=$(uname -a)
+numberversionlinex=$(uname -a)
+# Sh Script Programm
+dirhome="${PWD}"
+dirsource="Linex_NextCloud"
+scriptrun=$(basename -- "$0")
+scriptrunpy="LinexWeb.py"
+# Py Version
+pythonrun="python3.8"
+pythondir="Python-3.8.0"
+pythondownload="https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz"
+pythondownloadpip="https://bootstrap.pypa.io/get-pip.py"
+
+function function_python(){
+    apt update -y
+    sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libnl-3-dev libnl-genl-3-dev pkg-config libsqlite3-dev libpcre3-dev libffi-dev curl libreadline-dev ethtool libbz2-dev libtool autoconf -y
+	apt update -y
+    wget $pythondownload
+	tar -xf $pythondir.tgz
+	rm -r $pythondir.tgz
+	chmod -R 777 "$pythondir/"
+	cd $pythondir
+	./configure --enable-optimizations
+	make -j $(nproc)
+	sudo make altinstall
+	apt update -y
+    wget -O get-pip.py $pythondownloadpip
+    sudo $pythonrun get-pip.py
+	$pythonrun -m pip install --upgrade pip
+	$pythonrun --version
+	$pythonrun -m pip install requests
+	$pythonrun -m pip install alive-progress
+	$pythonrun -m pip install tqdm
+	$pythonrun -m pip install py7zr
+    $pythonrun -m pip install pylzma
+	$pythonrun -m pip install rarfile
+    $pythonrun -m pip install urllib3==1.26.7
+    cd ..
+    rm -r Python-3.8.0
+}
+
+function function_php(){
+    wget -O nextcloud.zip "https://drive.usercontent.google.com/download?id=1ORqNOY78I7E_DQ9O5ovQ8tsaDeoq_4uB&export=download&authuser=0&confirm=t&uuid=2d28c551-9fc8-473a-9cdb-b31ccdbce5e0&at=APZUnTVsY1IJ5Z8wteGrRslsvlfL%3A1709284650409"
+}
+
+function function_nginx(){
+    # nginx
+    sudo apt install nginx -y
+    sudo ufw app list
+    sudo ufw allow 'Nginx HTTP'
+    sudo ufw allow 22
+    sudo ufw allow 80
+    sudo ufw allow 443
+    echo "y" | sudo ufw enable
+    sudo ufw status
+    chmod -R 777 /var/www/html
+}
+
+# function function_projects(){
+#     # projects
+#     cd "$dirsource"
+#     mkdir projects
+#     mkdir projects_downloads
+#     sudo chmod -R 777 projects
+#     sudo chmod -R 777 projects_downloads
+#     cd ..
+# }
+
+function function_snapd(){
+    # Установка certbot для Авторегестрации HTTPS
+    sudo apt install snapd -y
+    sudo snap install core; sudo snap refresh core
+    sudo snap install --classic certbot
+    sudo ln -s /snap/bin/certbot /usr/bin/certbot
+    sudo systemctl stop nginx
+    sudo systemctl start nginx
+    sudo systemctl restart nginx
+}
+
+function function_tor(){
+    # Установка Tor Сети
+    sudo apt install tor -y
+}
+
+function function_nextcloud(){
+
+}
+
+function function_pack2(){
+    # Установка Пакетов
+    echo "Обновление..."
+    apt update -y
+    echo "Загрузка Пакетов 1..."
+    apt install sudo -y
+    apt install ssh -y
+    apt install curl -y
+    apt install wget -y
+    apt install git -y
+    apt install unzip -y
+    apt install rar -y
+    apt install zip -y
+    apt install p7zip-full -y
+	apt install unrar-free -y
+    apt update -y
+    echo "Загрузка Пакетов 2..."
+    function_python
+    function_core6
+    function_nginx
+    function_snapd
+    function_tor
+    #function_projects
+    echo "-----Конец Установки Пакетов-----"
+	echo "Авто Выход с Скрипта"
+	echo "Повторно Войдите в Скрипт Командой"
+	echo "bash ./$dirsource/$scriptrun"
+	exit
+}
+
+function access_linex(){
+	# Ubuntu полный доступ к папке
+	nameuser=$USER
+	chmod -R 777 "$dirsource/"
+}
+function auto_remove_program(){
+    # Автоудаление Программы
+    rm -rf "$dirsource"
+}
+# -----------------------------------------------------
+
+function main(){
+    # Основное Меню
+    echo "Команда: pack (Установка необходимых пакетов)"
+    echo "Команда: run (Запуск Скрипта)"
+    echo "Команда: remove (Авто Удаление Программы)"
+    echo "Команда: exit (Выход)"
+    echo "Введите Команду:"
+    read command
+    if [ "$command" == "remove" ]; then
+        echo "Вы Уверены в Удалении Программы Y/N"
+        read command2
+        if [ "$command2" == "y" ]; then
+            access_linex
+            auto_remove_program
+            echo "Программа $dirsource Удалена!"
+            exit
+        fi
+        if [ "$command2" == "Y" ]; then
+            access_linex
+            auto_remove_program
+            echo "Программа $dirsource Удалена!"
+            exit
+        fi
+	fi
+    if [ "$command" == "pack" ]; then
+        access_linex
+		function_pack2
+	fi
+    if [ "$command" == "run" ]; then
+        $pythonrun "./$dirsource/$scriptrunpy" "$1"
+	fi
+    if [ "$command" == "exit" ]; then
+		#break
+        exit
+	fi
+}
+
+while true
+do
+    current_time=$(date +%d.%m.%Y\ %T) # тикущая дата
+    echo "-------------------------$current_time--------------------------"
+    echo "Платформа: $distributivelinex"
+    #echo "Версия: $numberversionlinex"
+    access_linex
+    main "$distributivelinex"
+    #if [ "$distributivelinex" == "Ubuntu" ]; then
+        #access_linex
+        #main "$distributivelinex"
+    #fi
+    read -p "Нажмите Enter, чтобы продолжить"
+done
